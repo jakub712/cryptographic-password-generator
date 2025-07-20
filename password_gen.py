0603f744aa3c5caf3d4347eb9a89dda6b46ae609
JLesniak <JLesniak03@icloud.com>
2025-07-20 17:49:12 +0100

    Update: final password generator changes

--- password_gen.py
+++ password_gen.py
@@ -2,47 +2,49 @@ import secrets
 import time
 import hashlib
 import string
-import random
 import json
 import pyperclip
 
 #Random words, Cheers chat gpt#
 with open ("words.json", "r") as f:
     data = json.load(f)
+#Timer 1#
+start1 = time.perf_counter_ns()
 #symbols#
 symbols = "!@#$%^&*()-_=+"
-symbol1 = random.choice(symbols)
-symbol2 = random.choice(symbols)
+symbol1 = secrets.choice(symbols)
+symbol2 = secrets.choice(symbols)
+end1 = time.perf_counter_ns()
+
 
 #grabbing the time
-start = time.perf_counter_ns()
+start2 = time.perf_counter_ns()
 #generating a random num#
-for bits in [128]:
-    rand_num = secrets.randbits(bits)
+rand_num = secrets.randbits(1024)
+end2 = time.perf_counter_ns()
 #user input#
 user_input =  input("Write some text to further enchrypt the password! ")
 #making randbits and timing into a string#
-srandom = str(rand_num) + str(start)
+srandom = str(rand_num) + str(start1) + str(start2) + str(end1) + str(end2)
 #combining randbits timing and userinput to a string#
-combined = f"{user_input}{srandom}"
+combined = user_input + srandom
 #encoding the combo#
 encoded = combined.encode()
 #hashing it#
 hashed = hashlib.sha384(encoded).digest()
 #slicing the badboy#
-num = int.from_bytes(hashed[:3], "big") %100 +1
+num = int.from_bytes(hashed[:5], "big") %100 +1
 #a secure password#
-charset = string.ascii_lowercase + string.ascii_uppercase + string.punctuation
+charset = string.ascii_lowercase + string.ascii_uppercase + string.punctuation + string.digits
 word_list = data["words"]
-random.seed(hashed)
-password = f"{random.choice(word_list)}{symbol1}{num}{symbol2}{random.choice(word_list)}"
+passwordv0 = ''.join (charset[b % len(charset)]for b in hashed [:5])
+password = f"{secrets.choice(word_list)}{symbol1}{num}{symbol2}{passwordv0}{secrets.choice(word_list)}"
 #printing the password#
 print (password)
 #copy to clipbored#
 copy = input("Copy to clipboard? (y/n): ")
 if copy == "y":
-    import pyperclip
     pyperclip.copy(password)
     print("Password has been copied")
 else:
-    ("Have a good day. ")
+    print ("Have a good day. ")
